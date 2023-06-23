f7e53976ddc6e9e35c77f00e9d944f6b3bb06b0d
Tatyana Melnikova <t-melnikova96@mail.ru>
2023-06-23 14:34:33 +0300

    Реализация второго задания дипломного проекта

---
+++ src/test/java/EditUserDataTest.java
@@ -0,0 +1,89 @@
+import clients.UserClient;
+import dataprovider.UserProvider;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import pojo.CreateUserRequest;
+
+public class EditUserDataTest {
+    private UserClient userClient = new UserClient();
+    private String accessToken;
+
+    @Before
+    public void setUp() {
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+    }
+
+    @Test
+    @Step("Изменить информацию о пользователе (авторизован)")
+    @DisplayName("Успешное изменение информации о пользователе")
+    @Description("Статус кода: 200, В теле ответа есть success (true), изменен email и name")
+    public void successfulEditInfoUser() {
+        CreateUserRequest createUserRequest = UserProvider.getRandomCreateUserRequest();
+        //создание
+        accessToken = userClient.create(createUserRequest)
+                    .statusCode(200)
+                    .extract().jsonPath().get("accessToken");
+        //обновление информации
+        createUserRequest = UserProvider.getRandomCreateUserRequest();
+        userClient.edit(accessToken, createUserRequest)
+                .statusCode(200)
+                .body("success", Matchers.equalTo(true))
+                .and()
+                .body("user.email", Matchers.equalTo(createUserRequest.getEmail().toLowerCase()))
+                .and()
+                .body("user.name", Matchers.equalTo((createUserRequest.getName())));
+    }
+
+
+    @Test
+    @Step("Изменить информацию о пользователе (не авторизован)")
+    @DisplayName("Пользователь не обновляется, если он не авторизован")
+    @Description("Статус кода: 401, В теле ответа есть success (false), сообщение об ошибке, что пользователь должен быть авторизован")
+    public void unsuccessfulEditInfoUserWithoutAuth() {
+        CreateUserRequest createUserRequest = UserProvider.getRandomCreateUserRequest();
+        //попытка обновления информации без введенного токена
+        accessToken = "";
+        userClient.edit(accessToken, createUserRequest)
+                .statusCode(401)
+                .body("success", Matchers.equalTo(false))
+                .and()
+                .body("message", Matchers.equalTo("You should be authorised"));
+    }
+
+    @Test
+    @Step("Изменить email пользователя на другой, который уже указан у иного пользователя")
+    @DisplayName("Пользователь не обновляется, если введен email , который есть у другого пользователя")
+    @Description("Статус кода: 403, В теле ответа есть success (false), сообщение об ошибке, что пользователь с таким email уже существует")
+    public void unsuccessfulEditInfoUserWithExistEmail() {
+        //создаем пользователя 1
+        CreateUserRequest createUserRequest = UserProvider.getRandomCreateUserRequest();
+        accessToken = userClient.create(createUserRequest)
+                .statusCode(200)
+                .extract().jsonPath().get("accessToken");
+        //создаем пользователя 2
+        CreateUserRequest createUserOneRequest = UserProvider.getRandomCreateUserRequest();
+        userClient.create(createUserOneRequest);
+
+        //попытка обновления email
+        userClient.edit(accessToken, createUserOneRequest)
+                .statusCode(403)
+                .body("success", Matchers.equalTo(false))
+                .and()
+                .body("message", Matchers.equalTo("User with such email already exists"));
+    }
+    @After
+    public void tearDown() {
+        if(accessToken != null && accessToken != "") {
+            userClient.delete(accessToken)
+                    .statusCode(202);
+        }
+    }
+}
